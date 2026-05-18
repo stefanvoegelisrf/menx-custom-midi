6f0d4c60e1991cc772c3308fe4c8823d5fda7251
stefanvoegelisrf <stefan.voegeli@srf.ch>
2026-05-18 22:04:17 +0200

    add midi for keys

--- face_midi_controller/face_midi_controller.ino
+++ face_midi_controller/face_midi_controller.ino
@@ -1,5 +1,6 @@
 #include "Adafruit_seesaw.h"
 #include "Adafruit_NeoKey_1x4.h"
+#include <Control_Surface.h>
 
 #define I2C_SDA 14
 #define I2C_SCL 13
@@ -26,6 +27,8 @@
 
 #define ARRAY_SIZE(arr) (sizeof(arr) / sizeof(arr[0]))
 
+const uint8_t FIXED_TRIGGER_VELOCITY = 127;
+
 const uint8_t ENCODER_ADDRESSES[] = {
   ROTARY_EYE1_ADDRESS,
   ROTARY_EYE2_ADDRESS,
@@ -39,7 +42,7 @@ const char* ENCODER_NAMES[] = {
   "Eye 1", "Eye 2", "Nose 1", "Nose 2", "Ear 1", "Ear 2"
 };
 
-Adafruit_seesaw rotary_encoders[6] = {
+Adafruit_seesaw RotaryEncoders[6] = {
   Adafruit_seesaw(&Wire1),
   Adafruit_seesaw(&Wire1),
   Adafruit_seesaw(&Wire1),
@@ -48,7 +51,9 @@ Adafruit_seesaw rotary_encoders[6] = {
   Adafruit_seesaw(&Wire1)
 };
 
-int32_t encoder_positions[] = { 0, 0, 0, 0, 0, 0 };
+int32_t EncoderPositions[] = { 0, 0, 0, 0, 0, 0 };
+
+bool LastEncoderSwitchStates[] = { true, true, true, true, true, true };
 
 const uint8_t SLIDER_ADDRESSES[] = {
   SLIDER1_ADDRESS,
@@ -59,36 +64,40 @@ const char* SLIDER_NAMES[] = {
   "Eye Brow Slider 1", "Eye Brow Slider 2"
 };
 
-Adafruit_seesaw sliders[2] = {
+Adafruit_seesaw Sliders[2] = {
   Adafruit_seesaw(&Wire1),
   Adafruit_seesaw(&Wire1)
 };
 
-uint16_t slider_values[] = { 0, 0 };
+uint16_t SliderValues[] = { 0, 0 };
 
-Adafruit_NeoKey_1x4 neokey_buttons[KEY_ROWS][KEY_COLUMNS / 4] = {
+Adafruit_NeoKey_1x4 NeoKeyButtons[KEY_ROWS][KEY_COLUMNS / 4] = {
   { Adafruit_NeoKey_1x4(BUTTONS1_ADDRESS, &Wire1) },
   { Adafruit_NeoKey_1x4(BUTTONS2_ADDRESS, &Wire1) },
 };
 
-Adafruit_MultiNeoKey1x4 neokeys((Adafruit_NeoKey_1x4*)neokey_buttons, KEY_ROWS, KEY_COLUMNS / 4);
+Adafruit_MultiNeoKey1x4 NeoKeys((Adafruit_NeoKey_1x4*)NeoKeyButtons, KEY_ROWS, KEY_COLUMNS / 4);
+
+USBMIDI_Interface midi;
+
+NeoKey1x4Callback buttonPressed(keyEvent e);
 
 void setupEncoders() {
-  for (int i = 0; i < ARRAY_SIZE(rotary_encoders); i++) {
+  for (int i = 0; i < ARRAY_SIZE(RotaryEncoders); i++) {
     Serial.print("Initializing ");
     Serial.print(ENCODER_NAMES[i]);
     Serial.print(" at address 0x");
     Serial.println(ENCODER_ADDRESSES[i], HEX);
 
     // Initialize the encoder with its specific address
-    if (!rotary_encoders[i].begin(ENCODER_ADDRESSES[i])) {
+    if (!RotaryEncoders[i].begin(ENCODER_ADDRESSES[i])) {
       Serial.print("Error: Couldn't find encoder: ");
       Serial.println(ENCODER_NAMES[i]);
       while (1) delay(10);
     }
 
     // Check product firmware version
-    uint32_t version = ((rotary_encoders[i].getVersion() >> 16) & 0xFFFF);
+    uint32_t version = ((RotaryEncoders[i].getVersion() >> 16) & 0xFFFF);
     if (version != 4991) {
       Serial.print("Error: Wrong firmware loaded on ");
       Serial.print(ENCODER_NAMES[i]);
@@ -98,27 +107,27 @@ void setupEncoders() {
     }
 
     // Configure the built-in switch (using standard seesaw SS_SWITCH pin)
-    rotary_encoders[i].pinMode(ROTARY_SWITCH_PIN, INPUT_PULLUP);
+    RotaryEncoders[i].pinMode(ROTARY_SWITCH_PIN, INPUT_PULLUP);
 
     // Get starting position
-    encoder_positions[i] = rotary_encoders[i].getEncoderPosition();
+    EncoderPositions[i] = RotaryEncoders[i].getEncoderPosition();
 
     // Enable interrupts for the encoder
-    rotary_encoders[i].setGPIOInterrupts((uint32_t)1 << ROTARY_SWITCH_PIN, 1);
-    rotary_encoders[i].enableEncoderInterrupt();
+    RotaryEncoders[i].setGPIOInterrupts((uint32_t)1 << ROTARY_SWITCH_PIN, 1);
+    RotaryEncoders[i].enableEncoderInterrupt();
   }
 
   Serial.println("All encoders started successfully!\n");
 }
 
 void setupSliders() {
-  for (int i = 0; i < ARRAY_SIZE(sliders); i++) {
+  for (int i = 0; i < ARRAY_SIZE(Sliders); i++) {
     Serial.print("Initializing ");
     Serial.print(SLIDER_NAMES[i]);
     Serial.print(" at address 0x");
     Serial.println(SLIDER_ADDRESSES[i], HEX);
 
-    if (!sliders[i].begin(SLIDER_ADDRESSES[i])) {
+    if (!Sliders[i].begin(SLIDER_ADDRESSES[i])) {
       Serial.println(F("Slider not found!"));
       while (1) delay(10);
     }
@@ -126,7 +135,7 @@ void setupSliders() {
     uint16_t pid;
     uint8_t year, mon, day;
 
-    sliders[i].getProdDatecode(&pid, &year, &mon, &day);
+    Sliders[i].getProdDatecode(&pid, &year, &mon, &day);
     Serial.print("Slider found PID: ");
     Serial.print(pid);
     Serial.print(" datecode: ");
@@ -146,7 +155,7 @@ void setupSliders() {
 }
 
 void setupKeys() {
-  if (!neokeys.begin()) {  // start matrix
+  if (!NeoKeys.begin()) {  // start matrix
     Serial.println("Could not start NeoKeys, check wiring?");
     while (1) delay(10);
   }
@@ -156,7 +165,7 @@ void setupKeys() {
   // activate all keys and set callbacks
   for (int y = 0; y < KEY_ROWS; y++) {
     for (int x = 0; x < KEY_COLUMNS; x++) {
-      neokeys.registerCallback(x, y, buttonPressed);
+      NeoKeys.registerCallback(x, y, buttonPressed);
     }
   }
 }
@@ -171,56 +180,72 @@ void setup() {
   setupSliders();
   setupEncoders();
   setupKeys();
+  midi.begin();
 }
 
 void loop() {
   // Check all the encoders
-  for (int i = 0; i < ARRAY_SIZE(rotary_encoders); i++) {
+  for (int i = 0; i < ARRAY_SIZE(RotaryEncoders); i++) {
     // Read current position
-    int32_t new_position = rotary_encoders[i].getEncoderPosition();
+    int32_t newPosition = RotaryEncoders[i].getEncoderPosition();
 
     // If position changed, print it out
-    if (new_position != encoder_positions[i]) {
+    if (newPosition != EncoderPositions[i]) {
       Serial.print(ENCODER_NAMES[i]);
       Serial.print(" position: ");
-      Serial.println(new_position);
-      encoder_positions[i] = new_position;
+      Serial.println(newPosition);
+      EncoderPositions[i] = newPosition;
     }
 
     // Check if the encoder button is pressed
     // Note: The switch is active LOW (0 when pressed)
-    if (!rotary_encoders[i].digitalRead(ROTARY_SWITCH_PIN)) {
+    if (!RotaryEncoders[i].digitalRead(ROTARY_SWITCH_PIN)) {
       Serial.print(ENCODER_NAMES[i]);
       Serial.println(" BUTTON PRESSED!");
       delay(100);  // Simple debounce for testing
     }
   }
 
-  for (int i = 0; i < ARRAY_SIZE(sliders); i++) {
-    uint16_t slider_value = sliders[i].analogRead(SLIDER_ANALOGIN);
-    if (slider_value != slider_values[i]) {
+  for (int i = 0; i < ARRAY_SIZE(Sliders); i++) {
+    uint16_t sliderValue = Sliders[i].analogRead(SLIDER_ANALOGIN);
+    if (sliderValue != SliderValues[i]) {
       Serial.print(SLIDER_NAMES[i]);
       Serial.print(" position: ");
-      Serial.println(slider_value);
-      slider_values[i] = slider_value;
+      Serial.println(sliderValue);
+      SliderValues[i] = sliderValue;
     }
   }
 
-  neokeys.read();
+  NeoKeys.read();
 
+  midi.update();
   // don't overwhelm serial port
   delay(10);
 }
 
 NeoKey1x4Callback buttonPressed(keyEvent e) {
-  uint8_t key = e.bit.NUM;
+  uint8_t keyIndex = e.bit.NUM;
+
+  Channel midiChannel = (keyIndex < 4) ? Channel_1 : Channel_2;
+  uint8_t offset = (keyIndex < 4) ? 0 : 4;
+
+  uint8_t controlChangeNumber = 26 + (keyIndex - offset);
+  uint8_t noteNumber = 40 + (keyIndex - offset);
+
+  MIDIAddress midiAddressCc = MIDIAddress(controlChangeNumber, midiChannel);
+  MIDIAddress midiAddressNote = MIDIAddress(noteNumber, midiChannel);
+
   if (e.bit.EDGE == SEESAW_KEYPAD_EDGE_RISING) {
     Serial.print("Key press ");
-    Serial.println(key);
+    Serial.println(keyIndex);
+    midi.sendNoteOn(midiAddressNote, FIXED_TRIGGER_VELOCITY);
+    midi.sendCC(midiAddressCc, 127);
 
   } else if (e.bit.EDGE == SEESAW_KEYPAD_EDGE_FALLING) {
     Serial.print("Key release ");
-    Serial.println(key);
+    Serial.println(keyIndex);
+    midi.sendNoteOff(midiAddressNote, FIXED_TRIGGER_VELOCITY);
+    midi.sendCC(midiAddressCc, 0);
   }
 
   return 0;
